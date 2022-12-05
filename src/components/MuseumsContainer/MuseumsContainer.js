afb59125d9922a80bfc48fd29b3baadf40c6e401
Andrew Knapick <ajknapick@yahoo.com>
2022-12-04 19:56:48 -0600

    Hide URI

--- src/components/MuseumsContainer/MuseumsContainer.js
+++ src/components/MuseumsContainer/MuseumsContainer.js
@@ -4,24 +4,25 @@ import QueryResult from '../QueryResult/QueryResult';
 import { useQuery, gql } from '@apollo/client';
 
 const MUSEUMS_QUERY = gql`
-  query museums{
-    museums(placeId: "ChIJb13H9tx4bIcRPQoWtgSMyKk") {
+  
+    museums(city: "Denver", state: "CO", zipcode: "80202") {
      name
         rating
         placeId
         latitude
         longitude
     }
-  }
+  
 `;
 
 function MuseumsContainer({museums}) {
-  const {loading, error, data} = useQuery(museums)
-  console.log(data)
+    const {loading, error, data} = useQuery(MUSEUMS_QUERY)
+    console.log(data)
+  
 
   return (
     <QueryResult error={error} loading={loading} data={data}>
-
+      {console.log(data)}
       {/* <MuseumCard museums={museums}/> */}
     </QueryResult>
   )
