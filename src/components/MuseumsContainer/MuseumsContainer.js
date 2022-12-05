274de4ca02fde285dd4591acac64fc2d355daa38
Dinne Kopelevich <dinne22@hotmail.com>
2022-12-05 14:17:21 -0700

    Fix museum query

--- src/components/MuseumsContainer/MuseumsContainer.js
+++ src/components/MuseumsContainer/MuseumsContainer.js
@@ -8,16 +8,11 @@ import Map from "../Map/Map";
 import QueryResult from "../QueryResult/QueryResult";
 
 import museumsData from "../../testData/museumsData";
-// import { MATTS_API_KEY } from "../../secret";
+import { MATTS_API_KEY } from "../../secret";
 
 const MUSEUMS_QUERY = gql`
-<<<<<<< HEAD
-  {
-    museums(city: "Denver", state: "CO", zipcode: "80202") {
-=======
   query Museums($city: String!, $state: String!, $zipcode: String!) {
     museums(city: $city, state: $state, zipcode: $zipcode) {
->>>>>>> 1bdcd2529c169673ed47408ab4451608ec713e95
       placeId
       name
       rating
@@ -27,7 +22,6 @@ const MUSEUMS_QUERY = gql`
   }
 `;
 
-<<<<<<< HEAD
 // query: "{
 //   museums(city: "Denver", state: "CO", zipcode: "80202") {
 //       placeId
@@ -39,39 +33,12 @@ const MUSEUMS_QUERY = gql`
 // }"
 
 function MuseumsContainer() {
-  const { loading, error, data } = useQuery(MUSEUMS_QUERY);
-  console.log({ loading });
-  console.log({ data });
+  const { loading, error, data } = useQuery(MUSEUMS_QUERY, {
+    variables: { city: "Denver", state: "CO", zipcode: "80202" },
+  });
+  // console.log({ loading });
   console.log({ error });
-=======
-const SPOTIFY_QUERY = gql`
-  query getByArtist($name: String!) {
-    queryArtists (byName: $name) {
-      name
-      image
-      albums {
-          name
-      }
-    }
-  }
-`;
-
-function MuseumsContainer() {
-  const {loading, error, data} = useQuery(MUSEUMS_QUERY, { 
-    variables: { city: "Denver", state: "CO", zipcode: "80202" }
-  })
-
-  // const {loading, error, data} = useQuery(MUSEUMS_QUERY, { 
-  //   variables: { "name": "Beach Boys"} 
-  // })
-  console.log({loading})
-  console.log({data})
-  console.log({error})
-  console.log({MUSEUMS_QUERY})
-  console.log({SPOTIFY_QUERY})
-
-
->>>>>>> 1bdcd2529c169673ed47408ab4451608ec713e95
+  console.log({ data });
 
   const render = (Status) => {
     return <h1>{Status}</h1>;
@@ -83,10 +50,7 @@ function MuseumsContainer() {
         <Map />
       </Wrapper>
       <QueryResult error={error} loading={loading} data={data}>
-<<<<<<< HEAD
         {console.log({ data })}
-=======
->>>>>>> 1bdcd2529c169673ed47408ab4451608ec713e95
         {/* <MuseumCard data={data} /> */}
       </QueryResult>
     </>
