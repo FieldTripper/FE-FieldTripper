d4c1fda0f6979e39867adfc5c1ffe613ecd0ce01
Carissa Gross <carissamgross@gmail.com>
2022-12-05 19:17:46 -0700

    Update header component and add logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <div className='header'>
+             <img src={logo} alt="logo" className="logo" />
+            </div>
+    )
+}
+export default Header
