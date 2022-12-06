bd6e7c3a6839d4421877388c9266247a06c2120e
Carissa Gross <carissamgross@gmail.com>
2022-12-06 13:48:27 -0700

    Upodate footer component and styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p className='app-info'>Created By:</p>
+        </div>
+    )
+}
+export default Footer
