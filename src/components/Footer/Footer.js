c1fbe4980ae4f15400f43a99ec49a974dd9eb0ee
Carissa Gross <carissamgross@gmail.com>
2022-12-06 14:04:50 -0700

    Update footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <p className='app-info'>Created By:</p>
+            <p className='created-by'><b>Created By:</b></p>
+            <p className='creators'> Dinne Kopelevich | Carissa Gross | A.J. Krumholz | Andrew Knapick | Andrew Mullins | Mary Ballantyne | Matthew Press </p>
         </div>
     )
 }
