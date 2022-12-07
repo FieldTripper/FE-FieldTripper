e1020ead6da8dd43eed2125d00912aa69c6dd623
Dinne Kopelevich <dinne22@hotmail.com>
2022-12-06 18:08:29 -0700

    Add booking-form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,48 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom'
-import Header from '../Header/Header';
-import SearchForm from '../SearchForm/SearchForm';
-import MuseumsContainer from '../MuseumsContainer/MuseumsContainer';
-import Footer from '../Footer/Footer';
-import './App.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "../Header/Header";
+import SearchForm from "../SearchForm/SearchForm";
+import BookingForm from "../BookingForm/BookingForm";
+import MuseumsContainer from "../MuseumsContainer/MuseumsContainer";
+import Footer from "../Footer/Footer";
+import "./App.css";
 
 function App() {
-  const [values, setSearchTerms] = useState({city: '', state: '', zipCode: ''})
+  const [values, setSearchTerms] = useState({
+    city: "",
+    state: "",
+    zipCode: "",
+  });
 
   const updateSearch = (values) => {
-    setSearchTerms({ city: values.city, state: values.state, zipCode: values.zipCode })
-  }
+    setSearchTerms({
+      city: values.city,
+      state: values.state,
+      zipCode: values.zipCode,
+    });
+  };
+
+  const bookTrip = (values) => {
+    setSearchTerms({
+      museum: values.museum,
+      time: values.time,
+      people: values.people,
+    });
+  };
 
   return (
     <main>
       <Header />
       <Routes>
-        <Route path='/' element={<SearchForm updateSearch={updateSearch} />} />
-        <Route path='/museums' element={<MuseumsContainer queryValues={values} />} />
+        <Route path="/" element={<SearchForm updateSearch={updateSearch} />} />
+        <Route
+          path="/museums"
+          element={<MuseumsContainer queryValues={values} />}
+        />
+        <Route
+          path="/booking-form"
+          element={<BookingForm bookTrip={bookTrip} />}
+        />
       </Routes>
       <Footer />
     </main>
