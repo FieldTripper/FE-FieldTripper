c53bbe495215e733e34567885cf23cc8f471fa77
Carissa Gross <carissamgross@gmail.com>
2022-12-07 18:03:56 -0700

    Update about page and add links to GitHub and LinkedIn

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,12 +6,14 @@ const About = () => {
         <div className='about-page'>
             <p className='created-by'><b>CREATED BY</b></p>
             <p className='creators'> <b>Mary Ballantyne:</b> LinkedIn | GitHub<br></br>
-            <b>Carissa Gross:</b> LinkedIn | GitHub<br></br>
-            <b>Andrew Knapick:</b> LinkedIn | GitHub<br></br>
-            <b>Dinne Kopelevich:</b> LinkedIn | GitHub<br></br>
-            <b>A.J. Krumholz:</b> LinkedIn | GitHub<br></br>
-            <b>Andrew Mullins:</b> LinkedIn | GitHub<br></br>
-            <b>Matthew Press:</b> LinkedIn | GitHub </p>
+
+                <b>Carissa Gross:</b> <a href="https://www.linkedin.com/in/carissa-gross/">LinkedIn</a> | <a href="https://github.com/carissagross">GitHub</a><br></br>
+                <b>Andrew Knapick:</b> <a href="https://www.linkedin.com/in/andrew-knapick/">LinkedIn</a> | <a href="https://github.com/Universal-Patois">GitHub</a><br></br>
+                <b>Dinne Kopelevich:</b> <a href="https://www.linkedin.com/in/dinne-kopelevich-174584a/">LinkedIn</a> | <a href="https://github.com/DinneK">GitHub</a><br></br>
+                <b>A.J. Krumholz:</b> <a href="https://www.linkedin.com/in/aj-krumholz/">LinkedIn</a> | <a href="https://github.com/ajkrumholz">GitHub</a><br></br>
+                <b>Andrew Mullins:</b> <a href="https://www.linkedin.com/in/andrewmullins233/">LinkedIn</a> | <a href="https://github.com/mullinsand">GitHub</a><br></br>
+
+                <b>Matthew Press:</b> <a href="https://www.linkedin.com/in/matthew-press-813961246/">LinkedIn</a> | <a href="https://github.com/MatthewPress">GitHub</a><br></br></p>
         </div>
     )
 }
