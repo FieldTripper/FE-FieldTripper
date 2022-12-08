59afbf7d18fede9396c4bbe5e0e881fb92d5d13c
Andrew Knapick <ajknapick@yahoo.com>
2022-12-08 16:45:38 -0600

    Add conditional rendering for museum information

--- src/components/MuseumInfo/MuseumInfo.js
+++ src/components/MuseumInfo/MuseumInfo.js
@@ -38,24 +38,43 @@ function MuseumInfo() {
     <QueryResult error={error} loading={loading} data={data}>
       <section>
         <h1>{name}</h1>
-        
-        {/* <h2>{data.museum.address}</h2>
-        <img src={data.museum.imageUrl} alt={data.museum.imageDescription} width="500" height="600"/>
-        <h3>Rating: {data.museum.rating}/5</h3>
-        <h3>Total Ratings: {data.museum.totalRatings}</h3>
+
+        {data.museum.address && (
+          <h2>{data.museum.address}</h2>
+        )}
+
+        {data.museum.imageUrl && (
+          <img src={data.museum.imageUrl} alt={data.museum.imageDescription} width="500" height="600"/>
+        )}
+
+        {data.museum.rating && (
+          <h3>Rating: {data.museum.rating}/5</h3>
+        )}
+
+        {data.museum.totalRatings && (
+          <h3>Total Ratings: {data.museum.totalRatings}</h3>
+        )}
+
         {data.museum.price && (
             <h3>Price: {data.museum.price}</h3>
         )}
+
         {data.museum.wheelchairAccessibleEntrance && (
           <h3>Wheelchair Accessible: {data.museum.wheelchairAccessibleEntrance}</h3>
         )}
-        {data.museum.combinedHoo}
-        <ul>
-        {data.museum.combinedHoo.map((day) => (
-          <li key={day}>{day}</li>
-        ))}
-        </ul>
-        <a href={data.museum.website}>{data.museum.website}</a> */}
+
+        {data.museum.combinedHoo && (
+          <ul>
+          {data.museum.combinedHoo.map((day) => (
+            <li key={day}>{day}</li>
+          ))}
+          </ul>
+        )}
+
+        {data.museum.website && (
+          <a href={data.museum.website}>{data.museum.website}</a> 
+          )}
+       
        
       </section>
     </QueryResult>
