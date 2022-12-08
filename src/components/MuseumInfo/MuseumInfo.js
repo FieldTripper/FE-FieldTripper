f5694a1eec28ec984cfc25e0caa4dc9919825afe
Andrew Knapick <ajknapick@yahoo.com>
2022-12-08 17:52:39 -0600

    Create function to format address

--- src/components/MuseumInfo/MuseumInfo.js
+++ src/components/MuseumInfo/MuseumInfo.js
@@ -33,8 +33,9 @@ function MuseumInfo() {
     },
   });
   console.log('data', data)
+  console.log('loading', loading)
 
-  const displayPrice = (priceNumber) => {
+  const formatPrice = (priceNumber) => {
     if(priceNumber === 1) {
       return "$"
     } else if (priceNumber === 2) {
@@ -52,51 +53,64 @@ function MuseumInfo() {
     }
   }
 
+
+const formatAddress = (address) => {
+  const newAddress = address.split(',').map((span) => {
+    return span.slice(22)
+  }). map((span) => {
+    return span.slice(0, -7)
+  })
+  return newAddress
+}
+
   return (
     <QueryResult error={error} loading={loading} data={data}>
       <section>
-        <h1>{name}</h1>
+        {loading ? <p>Please Wait</p> :
+          <>
+            <h1>{name}</h1>
 
-        {data.museum.address && (
-          <h2>{data.museum.address}</h2>
-        )}
+            {data.museum.address && (
+              <h2>
+                {formatAddress(data.museum.address)}
+              </h2>
+            )}
 
-        {data.museum.imageUrl && (
-          <img src={data.museum.imageUrl} alt={data.museum.imageDescription} width="500" height="600"/>
-        )}
+            {data.museum.imageUrl && (
+              <img src={data.museum.imageUrl} alt={data.museum.imageDescription} width="500" height="600"/>
+            )}
 
-        {data.museum.rating && (
-          <h3>Rating: {data.museum.rating}/5</h3>
-        )}
+            {data.museum.rating && (
+              <h3>Rating: {data.museum.rating}/5</h3>
+            )}
 
-        {data.museum.totalRatings && (
-          <h3>Total Ratings: {data.museum.totalRatings}</h3>
-        )}
+            {data.museum.totalRatings && (
+              <h3>Total Ratings: {data.museum.totalRatings}</h3>
+            )}
 
-        {data.museum.price && (
-            <h3>Price: {displayPrice(data.museum.price)}</h3>
-        )}
+            {data.museum.price && (
+                <h3>Price: {formatPrice(data.museum.price)}</h3>
+            )}
 
-        {data.museum.wheelchairAccessibleEntrance && (
-          <h3>Wheelchair Accessible: {wheelchairAccessible(data.museum.wheelchairAccessibleEntrance)}</h3>
-        )}
+            {data.museum.wheelchairAccessibleEntrance && (
+              <h3>Wheelchair Accessible: {wheelchairAccessible(data.museum.wheelchairAccessibleEntrance)}</h3>
+            )}
 
-        {data.museum.combinedHoo && (
-          <ul>
-          {data.museum.combinedHoo.map((day) => (
-            <li key={day}>{day}</li>
-          ))}
-          </ul>
-        )}
+            {data.museum.combinedHoo && (
+              <ul>
+              {data.museum.combinedHoo.map((day) => (
+                <li key={day}>{day}</li>
+              ))}
+              </ul>
+            )}
 
-        {data.museum.website && (
-          <a href={data.museum.website}>{data.museum.website}</a> 
-          )}
-       
-       
+            {data.museum.website && (
+              <a href={data.museum.website}>{data.museum.website}</a> 
+              )}
+          </>
+        }
       </section>
     </QueryResult>
-
   )
 }
 
