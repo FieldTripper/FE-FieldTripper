27d303073015471729e66fc983dbd3fcf4357ccf
Carissa Gross <carissamgross@gmail.com>
2022-12-09 11:42:42 -0700

    Update museum info page

--- src/components/MuseumInfo/MuseumInfo.js
+++ src/components/MuseumInfo/MuseumInfo.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router";
 import QueryResult from "../QueryResult/QueryResult";
 import './MuseumInfo.css'
 
-const MUSEUM_QUERY = gql `
+const MUSEUM_QUERY = gql`
   query Museum($placeId: String!) {
     museum(placeId: $placeId) {
       placeId
@@ -26,7 +26,7 @@ const MUSEUM_QUERY = gql `
 `
 
 function MuseumInfo() {
-  const {placeId} = useParams()
+  const { placeId } = useParams()
 
   const { loading, error, data } = useQuery(MUSEUM_QUERY, {
     variables: {
@@ -37,7 +37,7 @@ function MuseumInfo() {
   console.log('loading', loading)
 
   const formatPrice = (priceNumber) => {
-    if(priceNumber === 1) {
+    if (priceNumber === 1) {
       return "$"
     } else if (priceNumber === 2) {
       return "$$"
@@ -47,33 +47,33 @@ function MuseumInfo() {
   }
 
   const wheelchairAccessible = (isAccessible) => {
-    if(isAccessible) {
+    if (isAccessible) {
       return "Yes"
     } else {
       return "No"
     }
   }
 
-// const formatAddress = (address) => {
-//   const newAddress = address.split(',').map((span) => {
-//     return span.slice(22)
-//   }). map((span) => {
-//     return span.slice(0, -7)
-//   })
-//   console.log(newAddress)
-//   return newAddress
-// }
+  // const formatAddress = (address) => {
+  //   const newAddress = address.split(',').map((span) => {
+  //     return span.slice(22)
+  //   }). map((span) => {
+  //     return span.slice(0, -7)
+  //   })
+  //   console.log(newAddress)
+  //   return newAddress
+  // }
 
-const formatAddress = (address) => {
-  console.log(address)
-  const newAddress1 = address.replaceAll('<span class="street-address">', "")
-  const newAddress2 = newAddress1.replaceAll('<span class="locality">', "")
-  const newAddress3 = newAddress2.replaceAll('<span class="region">', "")
-  const newAddress4 = newAddress3.replaceAll('<span class="postal-code">', "")
-  const newAddress5 = newAddress4.replaceAll('<span class="country-name">', "")
-  const newAddress6 = newAddress5.replaceAll('</span>', "")
-  return newAddress6
-}
+  const formatAddress = (address) => {
+    console.log(address)
+    const newAddress1 = address.replaceAll('<span class="street-address">', "")
+    const newAddress2 = newAddress1.replaceAll('<span class="locality">', "")
+    const newAddress3 = newAddress2.replaceAll('<span class="region">', "")
+    const newAddress4 = newAddress3.replaceAll('<span class="postal-code">', "")
+    const newAddress5 = newAddress4.replaceAll('<span class="country-name">', "")
+    const newAddress6 = newAddress5.replaceAll('</span>', "")
+    return newAddress6
+  }
 
   return (
     <QueryResult error={error} loading={loading} data={data}>
@@ -82,17 +82,18 @@ const formatAddress = (address) => {
           <>
             <h1>{data.museum.name}</h1><br></br>
 
-            {data.museum.address && (
-              <h2>
-                <b>Address:</b><br></br> {formatAddress(data.museum.address)}
-              </h2>
+            {/* <p className='address-title'><b>Address:</b></p><br></br> */}
+              {data.museum.address && (
+            <p className='address'>
+                { formatAddress(data.museum.address)} 
+            </p>
             )}
 
             {data.museum.imageUrl && (
-              <img src={data.museum.imageUrl} alt={data.museum.imageDescription} width="500" height="600"/>
+              <img className='museum-image' src={data.museum.imageUrl} alt={data.museum.imageDescription} width="500" height="600" />
             )}
 
-            {data.museum.rating && data.museum.rating > 0 ?(
+            {data.museum.rating && data.museum.rating > 0 ? (
               <h3><b>Rating:</b> {data.museum.rating}/5</h3>
             ) : <></>}
 
@@ -101,24 +102,25 @@ const formatAddress = (address) => {
             ) : <></>}
 
             {data.museum.price && (
-                <h3><b>Price:</b> {formatPrice(data.museum.price)}</h3>
+              <h3><b>Price:</b> {formatPrice(data.museum.price)}</h3>
             )}
 
             {data.museum.wheelchairAccessibleEntrance && (
-              <h3>Wheelchair Accessible: {wheelchairAccessible(data.museum.wheelchairAccessibleEntrance)}</h3>
+              <h3><b>Wheelchair Accessible:</b> {wheelchairAccessible(data.museum.wheelchairAccessibleEntrance)}</h3>
             )}
 
+            <h3><b>Hours:</b></h3>
             {data.museum.combinedHoo && (
-              <ul>
-              {data.museum.combinedHoo.map((day) => (
-                <li key={day}>{day}</li>
-              ))}
-              </ul>
+              <p>
+                {data.museum.combinedHoo.map((day) => (
+                  <p className='hours' key={day}>{day}</p>
+                ))}
+              </p>
             )}
 
             {data.museum.website && (
-              <a href={data.museum.website}>{data.museum.website}</a> 
-              )}
+              <a href={data.museum.website}>{data.museum.website}</a>
+            )}
           </>
         }
       </section>
