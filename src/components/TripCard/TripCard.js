4691d0d1c5549e078a3358c2643b387bdc9ef7d5
MatthewPress <press.matt14@gmail.com>
2022-12-12 15:07:31 -0700

    Feat: add delete trip functionality

--- src/components/TripCard/TripCard.js
+++ src/components/TripCard/TripCard.js
@@ -2,14 +2,15 @@ import { useMutation } from '@apollo/client';
 import { CREATE_USER_TRIP_MUTATION } from '../../queries/queries';
 import './TripCard.css';
 
-// import { DELETE_TRIP_MUTATION } from '../../queries/queries';
+import { DELETE_TRIP_MUTATION } from '../../queries/queries';
 
 function TripCard({ trip, user }) {
-  const [createUserTrip, { loading, error, data }] = useMutation(CREATE_USER_TRIP_MUTATION);
-  // const [deleteTrip, { loading, error, data }] = useMutation(DELETE_TRIP_MUTATION);
-  console.log({loading})
-  console.log({error})
+  const [createUserTrip, { loading, error }] = useMutation(CREATE_USER_TRIP_MUTATION);
+  const [deleteTrip, { data }] = useMutation(DELETE_TRIP_MUTATION);
+  // console.log({loading})
+  // console.log({error})
   console.log({data})
+  console.log(trip.id)
 
   return (
     <article className="trip-card" id={trip.id} key={trip.id} >
@@ -17,8 +18,7 @@ function TripCard({ trip, user }) {
       <p>{trip.destinationName}</p>
       <p>{trip.startTime}</p>
       <button onClick={() => createUserTrip({ variables: { userId: user.id, tripId: trip.id, isHost: false } })}>Join Trip</button>
-      {/* TESTING PURPOSES */}
-      {/* <button onClick={() => deleteTrip({ variables: { id: trip.id } })}>Delete Trip</button> */}
+      <button onClick={() => deleteTrip({ variables: { id: trip.id } })}>Delete Trip</button>
     </article>
   )
 }
