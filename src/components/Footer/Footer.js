5605185da4374b29401071d8cfe593ce2fb29ca2
Carissa Gross <carissamgross@gmail.com>
2022-12-13 19:51:07 -0700

    Update footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,20 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <Link style={{textDecoration: 'none'}} to='/'>
+            <Link style={{ textDecoration: 'none' }} to='/trip-type'>
                 <p className='home'>HOME</p>
             </Link>
-            <Link style={{textDecoration: 'none'}} to='/About'>
-            <p className='about'>ABOUT</p>
+            <Link style={{ textDecoration: 'none' }} to='/saved-trips'>
+                <p className='footer-saved-trips'>SAVED TRIPS</p>
+            </Link>
+            <Link style={{ textDecoration: 'none' }} to='/existing-trips'>
+                <p className='footer-join-a-trip'>JOIN A TRIP</p>
+            </Link>
+            <Link style={{ textDecoration: 'none' }} to='/search-form'>
+                <p className='footer-host-a-trip'>HOST A TRIP</p>
+            </Link>
+            <Link style={{ textDecoration: 'none' }} to='/About'>
+                <p className='footer-about'>ABOUT</p>
             </Link>
         </div>
     )
