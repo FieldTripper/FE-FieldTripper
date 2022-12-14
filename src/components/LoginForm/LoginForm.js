b8f026c2c54ce4b784cd13cfead8e4b6d958f2f5
Dinne Kopelevich <dinne22@hotmail.com>
2022-12-13 17:15:42 -0700

    Conditional rendering for museum info

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,60 +1,60 @@
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useLazyQuery } from '@apollo/client';
-import { CREATE_SESSION_QUERY } from '../../queries/queries';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
+import { CREATE_SESSION_QUERY } from "../../queries/queries";
 import { manageLocalData } from "../../utilities/utilities";
-import './LoginForm.css';
+import "./LoginForm.css";
 
 function LoginForm({ setUser }) {
   const [loginValues, setLoginValues] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
-  const [loginUser, { loading, error, data }] = useLazyQuery(CREATE_SESSION_QUERY);
+  const [loginUser, { loading, error, data }] =
+    useLazyQuery(CREATE_SESSION_QUERY);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (data) {
-      manageLocalData('userData', setUser, data.createSession.user);
+      manageLocalData("userData", setUser, data.createSession.user);
       navigate("/trip-type");
-    } 
+    }
   }, [data]);
 
   const handleChange = (event) => {
-    const fieldInput = event.target
+    const fieldInput = event.target;
     setLoginValues({ ...loginValues, [fieldInput.name]: fieldInput.value });
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     loginUser({ variables: loginValues });
-  }
+  };
 
   return (
     <section className="page--container column">
-      <h2>Login Form</h2>
-      <form onSubmit={(event) => handleSubmit(event)} >
+      <h2 className="login-form">Login Form</h2>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <input
+          className="login-form-input"
           type="email"
           name="email"
           placeholder="Email"
           onChange={(event) => handleChange(event)}
         />
         <input
+          className="login-form-input"
           type="password"
           name="password"
           placeholder="Password"
           onChange={(event) => handleChange(event)}
         />
-        <button 
-          className="primary--button"
-          type="submit"
-        >
+        <button className="primary--button" type="submit">
           Login
         </button>
       </form>
     </section>
-  )
+  );
 }
 
 export default LoginForm;
