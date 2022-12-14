1dba53468c9fe7b443296d8cfc11a1ac6b1ec4de
MatthewPress <press.matt14@gmail.com>
2022-12-14 00:52:02 -0700

    Refactor: change how dates are formated

--- src/components/UserSavedTripCard/UserSavedTripCard.js
+++ src/components/UserSavedTripCard/UserSavedTripCard.js
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client';
 import { DELETE_USER_TRIP_MUTATION } from "../../queries/queries";
 import { USER_TRIPS_QUERY } from "../../queries/queries";
+import { formatDates } from '../../utilities/utilities';
 import "./UserSavedTripCard.css";
 
 const UserSavedTripCard = ({
@@ -14,101 +15,6 @@ const UserSavedTripCard = ({
   tripName,
   user
 }) => {
-  console.log({startDate})
-  const newStartDate1 = startDate.slice(0, -10);
-  const newStartDate2 = newStartDate1.split("-");
-  const newStartTime = startDate.slice(11, -7);
-
-  const finalTripDate = [newStartDate2[1], newStartDate2[2], newStartDate2[0]];
-
-  const getDisplayedTime = () => {
-    let time;
-    if (newStartTime === "07") {
-      time = "7:00AM";
-    }
-    if (newStartTime === "08") {
-      time = "8:00AM";
-    }
-    if (newStartTime === "09") {
-      time = "9:00AM";
-    }
-    if (newStartTime === "10") {
-      time = "10:00AM";
-    }
-    if (newStartTime === "11") {
-      time = "11:00AM";
-    }
-    if (newStartTime === "12") {
-      time = "12:00PM";
-    }
-    if (newStartTime === "13") {
-      time = "1:00PM";
-    }
-    if (newStartTime === "14") {
-      time = "2:00PM";
-    }
-    if (newStartTime === "15") {
-      time = "3:00PM";
-    }
-    if (newStartTime === "16") {
-      time = "4:00PM";
-    }
-    if (newStartTime === "17") {
-      time = "5:00PM";
-    }
-    if (newStartTime === "18") {
-      time = "6:00PM";
-    }
-    if (newStartTime === "19") {
-      time = "7:00PM";
-    }
-    if (newStartTime === "20") {
-      time = "8:00PM";
-    }
-    return `${time}`;
-  };
-
-  const getDisplayedMonth = () => {
-    let month;
-    if (newStartDate2[1] === "01") {
-      month = "January";
-    }
-    if (newStartDate2[1] === "02") {
-      month = "February";
-    }
-    if (newStartDate2[1] === "03") {
-      month = "March";
-    }
-    if (newStartDate2[1] === "04") {
-      month = "April";
-    }
-    if (newStartDate2[1] === "05") {
-      month = "May";
-    }
-    if (newStartDate2[1] === "06") {
-      month = "June";
-    }
-    if (newStartDate2[1] === "07") {
-      month = "July";
-    }
-    if (newStartDate2[1] === "08") {
-      month = "August";
-    }
-    if (newStartDate2[1] === "09") {
-      month = "September";
-    }
-    if (newStartDate2[1] === "10") {
-      month = "October";
-    }
-    if (newStartDate2[1] === "11") {
-      month = "November";
-    }
-    if (newStartDate2[1] === "12") {
-      month = "December";
-    }
-    return `${month}`;
-  };
-
   const [deleteUserTrip] = useMutation(DELETE_USER_TRIP_MUTATION);
 
   const handleDelete = () => {
@@ -118,14 +24,18 @@ const UserSavedTripCard = ({
     ] });
   }
 
+  const returnedDate = formatDates(startDate, "MMMM D, YYYY");
+  const returnedTime = formatDates(startDate, "h:mmA");
+
   return (
     <div className="saved-trip-card">
       <p className="trip-name">{tripName}</p>
       <p className="saved-destination">{destinationName}</p>
       <p className="saved-trip-info">
-        <b>Your trip on:</b>
-        <br></br> {getDisplayedMonth()} {newStartDate2[2]}, {newStartDate2[0]}{" "}
-        starts at <b>{getDisplayedTime()}</b>
+        <b>Your trip on: </b>
+        {returnedDate} 
+        <br></br>
+         starts at <b>{returnedTime}</b>
       </p>
       <p className="attendance">
         {attendance} out of {maxAttendees} people are attending
